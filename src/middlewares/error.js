6fa64dcc1fdf2709437f9177825d609df4ff0819
drivezz22 <puwanut.pansailom@gmail.com>
2024-08-13 15:01:50 +0700

    init setup

---
+++ src/middlewares/error.js
@@ -0,0 +1,13 @@
+const { JsonWebTokenError, TokenExpiredError } = require("jsonwebtoken");
+
+const errorMiddleware = (err, req, res, next) => {
+  if (err instanceof JsonWebTokenError || err instanceof TokenExpiredError) {
+    return res.status(401).json({
+      message: err.message,
+    });
+  }
+  res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+    field: err.field,
+  });
+};
